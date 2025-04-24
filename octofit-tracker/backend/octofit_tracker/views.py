90b5e5aac6562a9f55d20bd561f883b333ffda18
lmthron <lthron@connexpay.com>
2025-04-24 18:24:27 +0000

    Updated settings.py and views.py to include codespace Django REST API endpoint suffix

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -1,7 +1,9 @@
 from django.http import JsonResponse
 
+from django.conf import settings
+
 def api_root(request):
     return JsonResponse({
         "message": "Welcome to the OctoFit API!",
-        "url": "https://miniature-eureka-pjxwr5969v539p6q.8000.app.github.dev",
+        "url": f"https://miniature-eureka-pjxwr5969v539p6q{settings.CODESPACE_API_SUFFIX}",
     })
